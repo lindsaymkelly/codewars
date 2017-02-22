4dbfecd8342e196b422c57d3be33a5a808cbae83
lindsaymkelly <lindzlady@gmail.com>
2017-02-21 23:22:45 -0500

    Refactor solution

--- comp_same.rb
+++ comp_same.rb
@@ -33,13 +33,13 @@
 def comp(array1, array2)
   status = true
   array1.each do |num|
-    if array2.include?(num*num) == false
-      status = false
-    end
+    status = false if !array2.include?(num*num)
   end
-  return status
+  status
 end
 
+
+# test code
 a = [121, 144, 19, 161, 19, 144, 19, 11]  
 b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 c = [121, 144, 19, 161, 19, 144, 19, 11]  
