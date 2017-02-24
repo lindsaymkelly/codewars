71bfb8861242b635da4526a602da6da932d54a7f
lindsaymkelly <lindzlady@gmail.com>
2017-02-24 16:37:18 -0500

    Add initial work to solution

--- odd_integer.rb
+++ odd_integer.rb
@@ -3,5 +3,7 @@
 # There will always be only one integer that appears an odd number of times.
 
 def find_it(seq)
-  #your code here
+  grouped_nums = seq.group_by{ |x| x }.values
 end
+
+p find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
