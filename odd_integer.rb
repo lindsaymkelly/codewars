749321cc4c0d382406821de02635560a8cf99d4e
lindsaymkelly <lindzlady@gmail.com>
2017-02-26 12:25:45 -0500

    Add initial working solution

--- odd_integer.rb
+++ odd_integer.rb
@@ -4,6 +4,11 @@
 
 def find_it(seq)
   grouped_nums = seq.group_by{ |x| x }.values
+  grouped_nums.each do |group|
+    if group.length % 2 != 0
+      return group 
+    end
+  end
 end
 
 p find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
