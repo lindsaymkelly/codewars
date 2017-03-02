8114ba363d0ce5c6d368e7ee546c5b9bb77bb214
lindsaymkelly <lindzlady@gmail.com>
2017-03-01 23:03:43 -0500

    Add initial working solution

--- nonrepeat.rb
+++ nonrepeat.rb
@@ -7,7 +7,30 @@
 # If a string contains all repeating characters, it should return the empty string ("").
 
 def  first_non_repeating_letter(s) 
-  grouped_letters = s.group_by{ |x| x }.values
+  chars = []
+  s.split(//).each do |character|
+    if chars.include?(character)
+      chars.delete(character)
+    else
+      chars << character 
+    end
+  end
+  return chars[0] 
 end
 
 p first_non_repeating_letter('stress')
+
+# def first_non_repeated_character(string):
+#   chars = []
+#   repeated = []
+#   for character in string:
+#     if character in chars:
+#       chars.remove(character)
+#       repeated.append(character)
+#     else:
+#       if not character in repeated:
+#         chars.append(character)
+#   if len(chars):
+#     return chars[0]
+#   else:
+#     return False
